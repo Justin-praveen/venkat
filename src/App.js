f9ae78a25ce10ead5f330a711d7538dd3aea0ad4
justinpraveen1417 <praveenc@thetopclassentertainment.com>
2022-10-06 16:44:55 +0530

    for login

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Sachin devsecops engineer...!
         </p>
         <a
           className="App-link"
